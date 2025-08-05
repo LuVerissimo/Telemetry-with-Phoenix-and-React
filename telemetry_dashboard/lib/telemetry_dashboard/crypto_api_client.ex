c4ddc1fde8470959eb6957be9a585ebda450a6a8
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2025-08-05 18:51:07 -0400

    api client module

---
+++ telemetry_dashboard/lib/telemetry_dashboard/crypto_api_client.ex
@@ -0,0 +1,19 @@
+defmodule TelemetryDashboard.CryptoApiClient do
+  @moduledoc """
+  Client for interacting with the Crypto API.
+  """
+
+  @base_url "https://api.coingecko.com/api/v3"
+
+  @doc """
+  Fetches the current price of a cryptocurrency by its ID.
+  """
+  def get_price(ids) do
+    ids_string = Enum.join(ids, ",")
+    Req.get!("#{@base_url}/simple/price", params: %{
+      ids: ids_string,
+      vs_currencies: "usd"
+    })
+    |> Req.Response.body()
+    end
+  end
